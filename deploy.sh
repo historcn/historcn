c964fe17d3762a3237516529e98fbce7ab00be85
Historcn <historcn@gmail.com>
2020-02-09 01:59:25 +0800

    update deploy.sh and readme

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,11 @@
 #!/usr/bin/env bash
 
 echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
-if [[ -z $1 ]]; then
+if [[ -z $@ ]]; then
   echo "Missing commit message"
   echo "Example: $0 'fixed typo error'"
   exit 1
 fi 
-
-[[ -z $@ ]] && echo "Missing commit message" && exit 1
-
 
 MSG="$@"
 HUGO_ENV="production"
@@ -17,7 +14,7 @@ hugo --gc -d docs || exit 1
 
 cd docs && scp ../README.md ./
 # Use another ssh key file
-ssh-agent bash -c "ssh-add /root/.ssh/id_historcn; 
+ssh-agent bash -c "ssh-add ~/.ssh/id_historcn; 
                     git init;
                     git remote add origin git@github.com:historcn/historcn.github.io.git;
                     git add .; 
@@ -25,7 +22,7 @@ ssh-agent bash -c "ssh-add /root/.ssh/id_historcn;
                     git push -u origin master --force"
 
 cd .. && rm -rf docs
-ssh-agent bash -c "ssh-add /root/.ssh/id_historcn; 
+ssh-agent bash -c "ssh-add ~/.ssh/id_historcn; 
                     git add .; 
-                    git commit -m $MSG; 
+                    git commit -m '$MSG'; 
                     git push -u origin master"
